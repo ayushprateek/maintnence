d5882e98617bdcfb7ab3a055186b9ff47e285cf8
Ayush Pratik <ayush.kishul@gmail.com>
2024-08-30 16:18:25 +0530

    Truck Layout

--- lib/zztest.dart
+++ lib/zztest.dart
@@ -1,29 +1,170 @@
 import 'dart:ui' as ui;
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:maintenance/Component/CustomColor.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
 
-class TruckDescription extends StatelessWidget {
-  ui.Image image;
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Custom drag'),
+      ),
+      body: Center(
+        child: MaterialButton(
+          onPressed: () async {
+            final ByteData data = await rootBundle.load('images/tyre.jpg');
+            ui.Image _image = await decodeImageFromList(data.buffer.asUint8List());
+            Get.to(() => TruckDescription(image: _image));
+          },
+          child: Text('TruckDescription'),
+        ),
+      ),
+    );
+  }
+}
+
+class TruckDescription extends StatefulWidget {
+  final ui.Image image;
 
   TruckDescription({required this.image});
 
   @override
-  Widget build(BuildContext context) {
+  _TruckDescriptionState createState() => _TruckDescriptionState();
+}
 
+class _TruckDescriptionState extends State<TruckDescription> {
+  List<Offset> tirePositions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initializeTirePositions();
+  }
+  void initializeTirePositions(){
+    int numHorizontalLines = 5;
+    double horizontalGap = Get.height / (numHorizontalLines + 1);
+    double verticalLineX = Get.width / 2;
+    double verticalLineTop = 0;
+    double verticalLineBottom = Get.height;
+
+    for (int i = 1; i <= numHorizontalLines; i++) {
+      double y = i * horizontalGap;
+
+
+
+      // Calculate the number of images per side
+      int numImages = i;
+      if (i == 4) {
+        numImages = 3;
+      } else if (i == 5) {
+        numImages = 2;
+      }
+
+      // Calculate spacing for images
+      double imageGap = Get.width / (2 * numImages + 1);
+
+      for (int j = 1; j <= numImages; j++) {
+        double xOffset = j * imageGap;
+
+        // Draw images on the left side
+        tirePositions.add(Offset(verticalLineX - xOffset, y - 20));
+
+        // Draw images on the right side
+        tirePositions.add(Offset(verticalLineX + xOffset - 40, y - 20));
+      }
+    }
+    // for (int i = 1; i <= numHorizontalLines; i++) {
+    //   double y = i * horizontalGap;
+    //
+    //   // Draw horizontal line
+    //   canvas.drawLine(Offset(0, y), Offset(size.width, y), paint);
+    //
+    //   // Calculate the number of images per side
+    //   int numImages = i;
+    //   if (i == 4) {
+    //     numImages = 3;
+    //   } else if (i == 5) {
+    //     numImages = 2;
+    //   }
+    //
+    //   // Calculate spacing for images
+    //   double imageGap = size.width / (2 * numImages + 1);
+    //
+    //   for (int j = 1; j <= numImages; j++) {
+    //     double xOffset = j * imageGap;
+    //
+    //     // Draw images on the left side
+    //     drawTireImage(canvas, Offset(verticalLineX - xOffset, y - 20), 40, 40);
+    //
+    //     // Draw images on the right side
+    //     drawTireImage(
+    //         canvas, Offset(verticalLineX + xOffset - 40, y - 20), 40, 40);
+    //   }
+    // }
+  }
+
+  void swapTires(int oldIndex, int newIndex) {
+    setState(() {
+      final temp = tirePositions[oldIndex];
+      tirePositions[oldIndex] = tirePositions[newIndex];
+      tirePositions[newIndex] = temp;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Truck Layout')),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CustomPaint(
-            size: Size(Get.width, Get.height/1.5),
-            painter: TruckLayoutPainter(image: image),
+        child: CustomPaint(
+          painter: LinePainter(),
+          child: Stack(
+            children: List.generate(tirePositions.length, (index) {
+              return Positioned(
+                left: tirePositions[index].dx,
+                top: tirePositions[index].dy,
+                child: Draggable<int>(
+                  data: index,
+                  feedback: Container(
+                    width: 50,
+                    height: 50,
+                    child: CustomPaint(
+                      size: Size(50, 50),
+                      painter: TirePainter(image: widget.image),
+                    ),
+                  ),
+                  childWhenDragging: Container(),
+                  child: DragTarget<int>(
+                    onAccept: (oldIndex) {
+                      swapTires(oldIndex, index);
+                    },
+                    builder: (context, candidateData, rejectedData) {
+                      return CustomPaint(
+                        size: Size(50, 50),
+                        painter: TirePainter(image: widget.image),
+                      );
+                    },
+                  ),
+                ),
+              );
+            }),
           ),
         ),
       ),
@@ -31,10 +172,41 @@ class TruckDescription extends StatelessWidget {
   }
 }
 
-class TruckLayoutPainter extends CustomPainter {
-  ui.Image image;
+class LinePainter extends CustomPainter {
 
-  TruckLayoutPainter({required this.image});
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = barColor
+      ..strokeWidth = 4.0;
+
+    double verticalLineX = Get.width / 2;
+    double verticalLineTop = 0;
+    double verticalLineBottom = Get.height;
+    int numHorizontalLines = 5;
+    double horizontalGap = Get.height / (numHorizontalLines + 1);
+
+    // Draw the vertical line
+    canvas.drawLine(Offset(verticalLineX, verticalLineTop),
+        Offset(verticalLineX, verticalLineBottom), paint);
+    for (int i = 1; i <= numHorizontalLines; i++) {
+      double y = i * horizontalGap;
+
+      // Draw horizontal line
+      canvas.drawLine(Offset(0, y), Offset(Get.width, y), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+class TirePainter extends CustomPainter {
+  final ui.Image image;
+
+  TirePainter({required this.image});
+
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -50,53 +222,8 @@ class TruckLayoutPainter extends CustomPainter {
     canvas.drawLine(Offset(verticalLineX, verticalLineTop),
         Offset(verticalLineX, verticalLineBottom), paint);
 
-    int numHorizontalLines = 5;
-    double horizontalGap = size.height / (numHorizontalLines + 1);
-
-    for (int i = 1; i <= numHorizontalLines; i++) {
-      double y = i * horizontalGap;
-
-      // Draw horizontal line
-      canvas.drawLine(Offset(0, y), Offset(size.width, y), paint);
-
-      // Calculate the number of images per side
-      int numImages = i;
-      if (i == 4) {
-        numImages = 3;
-      } else if (i == 5) {
-        numImages = 2;
-      }
-
-      // Calculate spacing for images
-      double imageGap = size.width / (2 * numImages + 1);
-
-      for (int j = 1; j <= numImages; j++) {
-        double xOffset = j * imageGap;
-
-        // Draw images on the left side
-        drawTireImage(canvas, Offset(verticalLineX - xOffset, y - 20), 40, 40);
-
-        // Draw images on the right side
-        drawTireImage(
-            canvas, Offset(verticalLineX + xOffset - 40, y - 20), 40, 40);
-      }
-    }
-  }
-
-  Future<void> drawTireImage(
-      Canvas canvas, Offset position, double width, double height) async {
-    // Rect rect = Rect.fromLTWH(position.dx, position.dy, width, height);
-    // Paint paint = Paint()..color = Colors.grey;
-
-    // Placeholder for tire image
-    // canvas.drawRect(rect, paint);
-
-    // You can replace the above code with image drawing using Image.asset
-    // For example:
-    // final ByteData data = await rootBundle.load('images/logo.png');
-    // final ui.Image image = await decodeImageFromList(data.buffer.asUint8List());
-    final Rect srcRect = Rect.fromLTWH(0, 0, 50, 50);
-    final Rect dstRect = Rect.fromLTWH(position.dx, position.dy, 50, 50);
+    final Rect srcRect = Rect.fromLTWH(0, 0, image.width.toDouble(), image.height.toDouble());
+    final Rect dstRect = Rect.fromLTWH(0, 0, size.width, size.height);
     canvas.drawImageRect(image, srcRect, dstRect, Paint());
   }
 
