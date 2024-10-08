5d4b47f2d9cf05102a6a7b20dddea10da5fe729d
Ayush Pratik <ayush.kishul@gmail.com>
2024-10-08 11:36:04 +0530

    Tyre swapping complete

--- lib/zztest.dart
+++ lib/zztest.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:maintenance/Component/CustomColor.dart';
+import 'package:maintenance/Component/CustomFont.dart';
 import 'package:maintenance/Component/GetBottomSheet.dart';
 import 'package:maintenance/Component/GetTextField.dart';
 import 'package:maintenance/DatabaseInitialization.dart';
@@ -163,12 +164,87 @@ class _TruckDescriptionState extends State<TruckDescription> {
     setState(() {});
   }
 
+  // void swapTires(int oldIndex, int newIndex) {
+  //   setState(() {
+  //     final temp = tirePositions[oldIndex];
+  //     tirePositions[oldIndex] = tirePositions[newIndex];
+  //     tirePositions[newIndex] = temp;
+  //
+  //     /// set old index
+  //     tirePositions[oldIndex].tyreCodeController.text = tirePositions[oldIndex].TyreCode ?? '';
+  //     tirePositions[oldIndex].serialNoController.text = tirePositions[oldIndex].SerialNo ?? '';
+  //     tirePositions[oldIndex].treadController.text = tirePositions[oldIndex].Tread?.toStringAsFixed(2) ?? '';
+  //     tirePositions[oldIndex].pressureController.text = tirePositions[oldIndex].Pressure ?? '';
+  //     tirePositions[oldIndex].remarksController.text = tirePositions[oldIndex].Remarks ?? '';
+  //     tirePositions[oldIndex].xAxlesController.text = tirePositions[oldIndex].XAxles?.toStringAsFixed(0) ?? '';
+  //     tirePositions[oldIndex].yTyresController.text = tirePositions[oldIndex].YTyres?.toStringAsFixed(0) ?? '';
+  //     tirePositions[oldIndex].zPositionController.text = tirePositions[oldIndex].ZPosition?.toStringAsFixed(0) ?? '';
+  //
+  //     /// set new index
+  //     tirePositions[newIndex].tyreCodeController.text = tirePositions[newIndex].TyreCode ?? '';
+  //     tirePositions[newIndex].serialNoController.text = tirePositions[newIndex].SerialNo ?? '';
+  //     tirePositions[newIndex].treadController.text = tirePositions[newIndex].Tread?.toStringAsFixed(2) ?? '';
+  //     tirePositions[newIndex].pressureController.text = tirePositions[newIndex].Pressure ?? '';
+  //     tirePositions[newIndex].remarksController.text = tirePositions[newIndex].Remarks ?? '';
+  //     tirePositions[newIndex].xAxlesController.text = tirePositions[newIndex].XAxles?.toStringAsFixed(0) ?? '';
+  //     tirePositions[newIndex].yTyresController.text = tirePositions[newIndex].YTyres?.toStringAsFixed(0) ?? '';
+  //     tirePositions[newIndex].zPositionController.text = tirePositions[newIndex].ZPosition?.toStringAsFixed(0) ?? '';
+  //
+  //     print('Hii');
+  //   });
+  // }
+
   void swapTires(int oldIndex, int newIndex) {
     setState(() {
       final temp = tirePositions[oldIndex];
       tirePositions[oldIndex] = tirePositions[newIndex];
       tirePositions[newIndex] = temp;
+
+      // Swap the controllers' text values
+      swapControllers(tirePositions[oldIndex], tirePositions[newIndex]);
     });
+  }
+
+  void swapControllers(MNVCL2 tire1, MNVCL2 tire2) {
+    // Swap tire codes
+    final tireCode1 = tire1.tyreCodeController.text;
+    tire1.tyreCodeController.text = tire2.tyreCodeController.text;
+    tire2.tyreCodeController.text = tireCode1;
+
+    // Swap serial numbers
+    final serialNo1 = tire1.serialNoController.text;
+    tire1.serialNoController.text = tire2.serialNoController.text;
+    tire2.serialNoController.text = serialNo1;
+
+    // Swap tread values
+    final tread1 = tire1.treadController.text;
+    tire1.treadController.text = tire2.treadController.text;
+    tire2.treadController.text = tread1;
+
+    // Swap pressure values
+    final pressure1 = tire1.pressureController.text;
+    tire1.pressureController.text = tire2.pressureController.text;
+    tire2.pressureController.text = pressure1;
+
+    // Swap remarks
+    final remarks1 = tire1.remarksController.text;
+    tire1.remarksController.text = tire2.remarksController.text;
+    tire2.remarksController.text = remarks1;
+
+    // Swap axle values
+    final xAxles1 = tire1.xAxlesController.text;
+    tire1.xAxlesController.text = tire2.xAxlesController.text;
+    tire2.xAxlesController.text = xAxles1;
+
+    // Swap tyre count
+    final yTyres1 = tire1.yTyresController.text;
+    tire1.yTyresController.text = tire2.yTyresController.text;
+    tire2.yTyresController.text = yTyres1;
+
+    // Swap Z position values
+    final zPosition1 = tire1.zPositionController.text;
+    tire1.zPositionController.text = tire2.zPositionController.text;
+    tire2.zPositionController.text = zPosition1;
   }
 
   @override
@@ -183,110 +259,92 @@ class _TruckDescriptionState extends State<TruckDescription> {
               return Positioned(
                 left: tirePositions[index].offset?.dx,
                 top: tirePositions[index].offset?.dy,
-                child: InkWell(
-                  onTap: () {
-                    getBottomSheet(
-                        context: Get.context!,
-                        content: SizedBox(
-                          width: Get.width,
-                          child: SingleChildScrollView(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const SizedBox(
-                                    height: 25,
+                child: Column(
+                  children: [
+                    getHeadingText(text: index.toString()),
+                    InkWell(
+                      onTap: () {
+                        getBottomSheet(
+                            context: Get.context!,
+                            content: SizedBox(
+                              width: Get.width,
+                              child: SingleChildScrollView(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      const SizedBox(
+                                        height: 25,
+                                      ),
+                                      getTextFieldWithoutLookup(
+                                          controller: tirePositions[index]
+                                              .tyreCodeController,
+                                          labelText: 'TyreCode',
+                                          height: 35),
+                                      getDisabledTextFieldWithoutLookup(
+                                          controller:
+                                              tirePositions[index].xAxlesController,
+                                          labelText: 'Axles',
+                                          height: 35),
+                                      getDisabledTextFieldWithoutLookup(
+                                          controller:
+                                              tirePositions[index].yTyresController,
+                                          labelText: 'Tyre',
+                                          height: 35),
+                                      getDisabledTextFieldWithoutLookup(
+                                          controller: tirePositions[index]
+                                              .zPositionController,
+                                          labelText: 'Position',
+                                          height: 35),
+                                      if (tirePositions[index]
+                                          .serialNoController
+                                          .text
+                                          .isEmpty)
+                                        getTextFieldWithoutLookup(
+                                            controller: tirePositions[index]
+                                                .serialNoController,
+                                            labelText: 'Serial No',
+                                            height: 35)
+                                      else
+                                        getDisabledTextFieldWithoutLookup(
+                                            controller: tirePositions[index]
+                                                .serialNoController,
+                                            labelText: 'Serial No',
+                                            height: 35),
+                                      getTextFieldWithoutLookup(
+                                          controller:
+                                              tirePositions[index].treadController,
+                                          labelText: 'Tread',
+                                          height: 35),
+                                      getTextFieldWithoutLookup(
+                                          controller: tirePositions[index]
+                                              .pressureController,
+                                          labelText: 'Pressure',
+                                          height: 35),
+                                      getTextFieldWithoutLookup(
+                                          controller: tirePositions[index]
+                                              .remarksController,
+                                          labelText: 'Remarks',
+                                          height: 35),
+                                    ],
                                   ),
-                                  getTextFieldWithoutLookup(
-                                      controller: tirePositions[index]
-                                          .tyreCodeController,
-                                      labelText: 'TyreCode',
-                                      height: 35),
-                                  getDisabledTextFieldWithoutLookup(
-                                      controller:
-                                          tirePositions[index].xAxlesController,
-                                      labelText: 'Axles',
-                                      height: 35),
-                                  getDisabledTextFieldWithoutLookup(
-                                      controller:
-                                          tirePositions[index].yTyresController,
-                                      labelText: 'Tyre',
-                                      height: 35),
-                                  getDisabledTextFieldWithoutLookup(
-                                      controller: tirePositions[index]
-                                          .zPositionController,
-                                      labelText: 'Position',
-                                      height: 35),
-                                  if (tirePositions[index]
-                                      .serialNoController
-                                      .text
-                                      .isEmpty)
-                                    getTextFieldWithoutLookup(
-                                        controller: tirePositions[index]
-                                            .serialNoController,
-                                        labelText: 'Serial No',
-                                        height: 35)
-                                  else
-                                    getDisabledTextFieldWithoutLookup(
-                                        controller: tirePositions[index]
-                                            .serialNoController,
-                                        labelText: 'Serial No',
-                                        height: 35),
-                                  getTextFieldWithoutLookup(
-                                      controller:
-                                          tirePositions[index].treadController,
-                                      labelText: 'Tread',
-                                      height: 35),
-                                  getTextFieldWithoutLookup(
-                                      controller: tirePositions[index]
-                                          .pressureController,
-                                      labelText: 'Pressure',
-                                      height: 35),
-                                  getTextFieldWithoutLookup(
-                                      controller: tirePositions[index]
-                                          .remarksController,
-                                      labelText: 'Remarks',
-                                      height: 35),
-                                ],
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                        height: 2 * Get.height / 3);
-                  },
-                  child: Draggable<int>(
-                    data: index,
-                    feedback: Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: tirePositions[index]
-                                      .serialNoController
-                                      .text
-                                      .isEmpty
-                                  ? Colors.red
-                                  : Colors.blue,
-                              width: 1.5)),
-                      child: CustomPaint(
-                        size: Size(50, 50),
-                        painter: TirePainter(image: widget.image),
-                      ),
-                    ),
-                    childWhenDragging: Container(),
-                    child: DragTarget<int>(
-                      onAccept: (oldIndex) {
-                        swapTires(oldIndex, index);
+                            height: 2 * Get.height / 3);
                       },
-                      builder: (context, candidateData, rejectedData) {
-                        return Container(
+                      child: Draggable<int>(
+                        data: index,
+                        feedback: Container(
+                          width: 50,
+                          height: 50,
                           decoration: BoxDecoration(
                               border: Border.all(
                                   color: tirePositions[index]
-                                      .serialNoController
-                                      .text
-                                      .isEmpty
+                                          .serialNoController
+                                          .text
+                                          .isEmpty
                                       ? Colors.red
                                       : Colors.blue,
                                   width: 1.5)),
@@ -294,10 +352,33 @@ class _TruckDescriptionState extends State<TruckDescription> {
                             size: Size(50, 50),
                             painter: TirePainter(image: widget.image),
                           ),
-                        );
-                      },
+                        ),
+                        childWhenDragging: Container(),
+                        child: DragTarget<int>(
+                          onAccept: (oldIndex) {
+                            swapTires(oldIndex, index);
+                          },
+                          builder: (context, candidateData, rejectedData) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: tirePositions[index]
+                                          .serialNoController
+                                          .text
+                                          .isEmpty
+                                          ? Colors.red
+                                          : Colors.blue,
+                                      width: 1.5)),
+                              child: CustomPaint(
+                                size: Size(50, 50),
+                                painter: TirePainter(image: widget.image),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               );
             }),
